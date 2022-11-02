0a172eaea19a3b51f2fedf14eb17b7a62d178c32
islamshu12 <islamshu12@gmail.com>
2022-11-02 13:08:30 +0200

    ي

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/',function(){
     return view('welcome');
+    
 });
 Route::get('rss_feed/{id}',[HomeController::class,'rss_feed'])->name('rss_feed');
