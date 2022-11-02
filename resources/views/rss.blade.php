2ca45f53b4ba4bd10a01afd04168bbbd20649b1d
islamshu12 <islamshu12@gmail.com>
2022-11-02 13:53:46 +0200

    edit

--- resources/views/rss.blade.php
+++ resources/views/rss.blade.php
@@ -1,11 +1,10 @@
 @php
     $currentURL = url()->full();
 @endphp
-<?=
-'<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
-?>
+<?= '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL ?>
 <rss version="2.0">
     <channel>
+        <author>{{ $user->name }}</author>
         <title>{{ $user->first_name }} {{ $user->last_name }}</title>
         <link>https://www.arabicreators.com</link>
         <description>arabicreators</description>
@@ -15,18 +14,15 @@
             <url>https://i1.sndcdn.com/avatars-Pghd90aexdU8iziD-D0ullg-original.jpg</url>
             <title>بزنس على الطريق | مع الغندور</title>
             <link>https://mghandour.com/</link>
-          </image>
-          <author>author@w3schools.com</author>
-          @foreach($sounds as $post)
+        </image>
+        @foreach ($sounds as $post)
             <item>
+                <author>{{ $user->name }}</author>
                 <title>{{ $post->title }}</title>
-                <link>{{ asset('public/audio/'.$post->sound) }}</link>
-                <enclosure type="audio/mpeg" url="{{ asset('public/audio/'.$post->sound) }}" length="2444"/>
+                <link>{{ asset('public/audio/' . $post->sound) }}</link>
+                <enclosure type="audio/mpeg" url="{{ asset('public/audio/' . $post->sound) }}" length="2444" />
                 <description>{!! 'تجربة' !!}</description>
-                <author>{{ $user->first_name }}.{{ $user->email }}</author>
-                <guid isPermaLink="false">{{ $post->id }}</guid>
-            
-
+                <guid>{{ asset('public/audio/' . $post->sound) }}</guid>
                 <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
             </item>
         @endforeach
