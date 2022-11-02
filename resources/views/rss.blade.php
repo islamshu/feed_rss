5a559a2786768db2271bb82eb622c7220c4b2e43
islamshu12 <islamshu12@gmail.com>
2022-11-02 13:14:10 +0200

    d

--- resources/views/rss.blade.php
+++ resources/views/rss.blade.php
@@ -4,7 +4,7 @@
 <rss version="2.0">
     <channel>
         <title>{{ $user->first_name }} {{ $user->last_name }}</title>
-        <link>sub.arabicreators.com</link>
+        <link>arabicreators.com</link>
         <description>arabicreators</description>
         <language>en</language>
         <pubDate>{{ now() }}</pubDate>
@@ -15,7 +15,7 @@
                 <link>{{ asset('public/audio/'.$post->sound) }}</link>
                 <enclosure type="audio/mpeg" url="{{ asset('public/audio/'.$post->sound) }}" length=""/>
                 <description>{!! 'تجربة' !!}</description>
-                <author>{{ $user->name }}</author>
+                <author>{{ $user->email }}</author>
                 <guid>{{ $post->id }}</guid>
                 <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
             </item>
